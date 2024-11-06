45b8fe0edc432a27ea456b28e0a65c4826d536b3
DerekAyalaDev <derek.ayala@encora.com>
2024-11-06 14:56:52 -0600

    Create DTO for Airport Information
    - name (String): The full name of the airport (e.g., "Los Angeles International Airport").
    - code (String): The IATA code of the airport (e.g., "LAX").

---
+++ Backend/src/main/java/com/encora/searchflights/dto/AirportInfoDTO.java
@@ -0,0 +1,11 @@
+package com.encora.searchflights.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+class AirportInfoDTO {
+    private String name;
+    private String code;
+}
