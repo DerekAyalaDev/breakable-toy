d848c6f1b07863fd4934364df0e68590a12323ce
DerekAyalaDev <derek.ayala@encora.com>
2024-11-06 14:57:21 -0600

    Create DTO for amenity
    This DTO represents a service or amenity offered on a flight, such as Wi-Fi, onboard entertainment, meals, etc.
    - name (String): The name of the service or amenity (e.g., "Wi-Fi", "Meal Service").
    - isChargeable (boolean): Indicates if the service has an additional cost (true for chargeable, false for free).

---
+++ Backend/src/main/java/com/encora/searchflights/dto/AmenityDTO.java
@@ -0,0 +1,11 @@
+package com.encora.searchflights.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AmenityDTO {
+    private String name;
+    private boolean isChargeable;
+}
