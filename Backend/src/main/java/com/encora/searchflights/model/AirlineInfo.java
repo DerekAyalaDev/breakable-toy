f708364907eaaa9b74c77a3540efc14ee7223b47
DerekAyalaDev <derek.ayala@encora.com>
2024-11-07 11:48:42 -0600

    Add JSON property mappings to AirlineInfo for Amadeus API compatibility
    - Added @JsonProperty annotations to AirlineInfo fields to ensure correct mapping with Amadeus API response.
    - Mapped "commonName" from the JSON response to "name" field in AirlineInfo for displaying airline name.
    - Mapped "iataCode" from the JSON response to "code" field in AirlineInfo for representing the airline IATA code.
    - This mapping ensures that data from the Amadeus API is accurately deserialized into the AirlineInfo model.

--- Backend/src/main/java/com/encora/searchflights/model/AirlineInfo.java
+++ Backend/src/main/java/com/encora/searchflights/model/AirlineInfo.java
@@ -1,11 +1,15 @@
 package com.encora.searchflights.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class AirlineInfo {
+    @JsonProperty("commonName")
     private String name;
+
+    @JsonProperty("iataCode")
     private String code;
 }
