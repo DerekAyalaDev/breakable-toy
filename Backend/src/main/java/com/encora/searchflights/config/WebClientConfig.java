c75587a4535d88f891d9ba4b38ff11b3289fe4c7
DerekAyalaDev <derek.ayala@encora.com>
2024-11-07 17:46:04 -0600

    update web client config to get access token

--- Backend/src/main/java/com/encora/searchflights/config/WebClientConfig.java
+++ Backend/src/main/java/com/encora/searchflights/config/WebClientConfig.java
@@ -1,9 +1,11 @@
 package com.encora.searchflights.config;
 
+import com.encora.searchflights.model.response.TokenResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class WebClientConfig {
@@ -11,8 +13,35 @@ public class WebClientConfig {
     @Value("${amadeus.api.base-url}")
     private String amadeusBaseUrl;
 
+    @Value("${amadeus.api.token-url}")
+    private String amadeusTokenUrl;
+
+    @Value("${amadeus.api.key}")
+    private String apiKey;
+
+    @Value("${amadeus.api.secret}")
+    private String apiSecret;
+
     @Bean
     public WebClient webClient(WebClient.Builder builder) {
-        return builder.baseUrl(amadeusBaseUrl).defaultHeader("Content-Type", "application/json").build();
+        return builder
+                .baseUrl(amadeusBaseUrl)
+                .defaultHeader("Content-Type", "application/json")
+                .build();
+    }
+
+    public Mono<String> getAccessToken() {
+        return WebClient.builder()
+                .baseUrl(amadeusTokenUrl)
+                .build()
+                .post()
+                .uri(uriBuilder -> uriBuilder
+                        .queryParam("grant_type", "client_credentials")
+                        .queryParam("client_id", apiKey)
+                        .queryParam("client_secret", apiSecret)
+                        .build())
+                .retrieve()
+                .bodyToMono(TokenResponse.class)
+                .map(TokenResponse::getAccessToken);
     }
 }
