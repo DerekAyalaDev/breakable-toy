0a111ec328a5f583eedfc210f310993fe138216d
DerekAyalaDev <derek.ayala@encora.com>
2024-11-07 17:48:31 -0600

    update where is the Airline info class is

---
+++ Backend/src/main/java/com/encora/searchflights/model/airline/AirlineInfo.java
@@ -0,0 +1,12 @@
+package com.encora.searchflights.model.airline;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AirlineInfo {
+
+    private String name; // Airline name
+    private String code; // Airline IATA code
+}
