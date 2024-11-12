65fe8bc985bc5565d12bc5b24b4cd915723b752b
DerekAyalaDev <derek.ayala@encora.com>
2024-11-12 12:21:00 -0600

    Integrate dotenv for loading API credentials in WebClientConfig
    - Configured java-dotenv to load API credentials from the .env file.
    - Added methods to retrieve apiKey and apiSecret as beans for easy access across the application.
    - Updated WebClientConfig to use application properties for base URL.

--- Backend/src/main/java/com/encora/searchflights/config/WebClientConfig.java
+++ Backend/src/main/java/com/encora/searchflights/config/WebClientConfig.java
@@ -1,7 +1,7 @@
 package com.encora.searchflights.config;
 
 import com.encora.searchflights.model.response.TokenResponse;
-import org.springframework.beans.factory.annotation.Value;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
@@ -12,35 +12,45 @@ import reactor.core.publisher.Mono;
 @Configuration
 public class WebClientConfig {
 
-    @Value("${amadeus.api.base-url}")
-    private String amadeusBaseUrl;
+    private final Dotenv dotenv = Dotenv.load();
 
-    @Value("${amadeus.api.token-url}")
-    private String amadeusTokenUrl;
+    @Bean
+    public String amadeusBaseUrl() {
+        return dotenv.get("AMADEUS_BASE_URL");
+    }
 
-    @Value("${amadeus.api.key}")
-    private String apiKey;
+    @Bean
+    public String apiKey() {
+        return dotenv.get("AMADEUS_API_KEY");
+    }
 
-    @Value("${amadeus.api.secret}")
-    private String apiSecret;
+    @Bean
+    public String apiSecret() {
+        return dotenv.get("AMADEUS_API_SECRET");
+    }
+
+    @Bean
+    public String amadeusTokenUrl() {
+        return dotenv.get("AMADEUS_TOKEN_URL");
+    }
 
     @Bean
     public WebClient webClient(WebClient.Builder builder) {
         return builder
-                .baseUrl(amadeusBaseUrl)
+                .baseUrl(amadeusBaseUrl())
                 .defaultHeader("Content-Type", "application/json")
                 .build();
     }
 
     public Mono<String> getAccessToken() {
         return WebClient.builder()
-                .baseUrl(amadeusTokenUrl)
+                .baseUrl(amadeusTokenUrl())
                 .build()
                 .post()
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .body(BodyInserters.fromFormData("grant_type", "client_credentials")
-                        .with("client_id", apiKey)
-                        .with("client_secret", apiSecret))
+                        .with("client_id", apiKey())
+                        .with("client_secret", apiSecret()))
                 .retrieve()
                 .bodyToMono(TokenResponse.class)
                 .map(TokenResponse::getAccessToken);
