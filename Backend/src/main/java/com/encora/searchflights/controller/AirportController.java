ca4ec57c0c8bfd9c054908b6d18d75cdf2406091
DerekAyalaDev <derek.ayala@encora.com>
2024-11-12 16:47:57 -0600

    Add new endpoint for fetching airport details by IATA code
    Added a new endpoint `/api/airports/get` in `AirportController` to provide a synchronous way to fetch airport details by IATA code.

--- Backend/src/main/java/com/encora/searchflights/controller/AirportController.java
+++ Backend/src/main/java/com/encora/searchflights/controller/AirportController.java
@@ -4,6 +4,7 @@ import com.encora.searchflights.model.airport.AirportInfo;
 import com.encora.searchflights.service.AirportService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -12,11 +13,17 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("api/airports")
 public class AirportController {
     private final AirportService airportService;
 
-    @GetMapping("api/airports/search")
+    @GetMapping("/search")
     public Mono<List<AirportInfo>> searchAirports(@RequestParam String keyword) {
         return airportService.searchAirportsByName(keyword);
+    }
+
+    @GetMapping("/get")
+    public AirportInfo getAirportByIataCode(@RequestParam String iataCode) {
+        return airportService.getAirportByIataCode(iataCode);
     }
 }
